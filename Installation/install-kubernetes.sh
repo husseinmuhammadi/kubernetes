08fdce93c9ea27dc1402dce5b92d1cc8fbf6ba5d
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-08-20 20:00:10 +0200

    pwd

--- Installation/install-kubernetes.sh
+++ Installation/install-kubernetes.sh
@@ -36,3 +36,15 @@ go version
 # Compile cri-docker
 sudo apt-get install make
 cd ~/cri-dockerd && make cri-dockerd
+
+pwd
+
+# Install cri-docker
+#cd ~/cri-dockerd
+#mkdir -p /usr/local/bin
+#install -o root -g root -m 0755 cri-dockerd /usr/local/bin/cri-dockerd
+#install packaging/systemd/* /etc/systemd/system
+#sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+#systemctl daemon-reload
+#systemctl enable cri-docker.service
+#systemctl enable --now cri-docker.socket
